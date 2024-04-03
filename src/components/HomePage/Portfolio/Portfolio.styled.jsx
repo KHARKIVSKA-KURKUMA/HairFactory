b811b7f8dbc94f4e0aecfb68165bbb653f350ff0
KHARKIVSKA-KURKUMA <catherine.vasyuk11@gmail.com>
2024-04-03 22:29:19 +0300

    updated portfolio

--- src/components/HomePage/Portfolio/Portfolio.styled.jsx
+++ src/components/HomePage/Portfolio/Portfolio.styled.jsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
+import { desktop, smallDesktop, tablet } from "../../../utils/viewpoints";
 
 const Container = styled.div`
-  padding: 64px 0;
+  display: none;
+  ${tablet} {
+    display: block;
+    padding: 64px 0;
+  }
 `;
 const Wrap = styled.div`
   display: flex;
@@ -9,28 +14,45 @@ const Wrap = styled.div`
   justify-content: center;
 `;
 const CenterImg = styled.img`
-  max-width: 398px;
-  max-height: 446px;
   border-radius: 24px;
+  max-width: 245px;
+  height: 296px;
+  ${smallDesktop} {
+    max-width: 350px;
+    height: 446px;
+  }
+  ${desktop} {
+    width: 398px;
+  }
 `;
 const SideWrap = styled.div`
   display: flex;
   gap: 16px;
   flex-direction: column;
   img {
-    max-width: 370px;
-    max-height: 215px;
     border-radius: 24px;
+    width: 245px;
+    height: 140px;
+    ${smallDesktop} {
+      width: 350px;
+      height: 215px;
+    }
+    ${desktop} {
+      width: 370px;
+    }
   }
 `;
 const Title = styled.h2`
   font-family: "Oranienbaum";
-  text-align: center;
-  letter-spacing: -0.015em;
-  margin-bottom: 64px;
   font-weight: 400;
-  font-size: 72px;
-  line-height: 0%.85;
+  font-size: 44px;
+  line-height: 0.85;
+  letter-spacing: -0.015em;
+  margin-bottom: 44px;
+  text-align: center;
+  ${smallDesktop} {
+    font-size: 56px;
+  }
 `;
 
 export { Container, Wrap, SideWrap, CenterImg, Title };
